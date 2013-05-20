427c62c9f634ffa09b302135d7c95f314af80e91
dagfinnr <dagfinn@startsiden.no>
2013-05-20 14:36:55 +0200

    Variable class somewhat simplified with new approach

--- lib/PPIx/EditorTools/ExtractMethod/Variable.pm
+++ lib/PPIx/EditorTools/ExtractMethod/Variable.pm
@@ -5,15 +5,10 @@ has 'id'   => ( is => 'ro', isa => 'Str' );
 has 'type'   => ( is => 'ro', isa => 'Str', required => 1 );
 
 has 'used_after' => ( is => 'rw', isa => 'Bool', default => 0 );
+has 'declared_in_selection' => ( is => 'rw', isa => 'Bool', default => 0 );
 
-has 'declared_in_scope'   => ( 
-    is => 'rw',
-    isa => 'Str',
-);
-
-sub used_in_scope {
-    my ($self, $scope) = @_;
-    return if ! $scope;
-    $self->used_in_scopes->insert($scope);
+sub make_reference {
+    return '\\' . $_[0]->id;
 }
+
 1;
